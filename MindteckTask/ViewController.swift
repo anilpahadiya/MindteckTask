f969354c3294b6a3a3680843036e645c4b23300a
anilpahadiya <anilkumarpahadiya@gmail.com>
2023-02-03 22:08:41 +0530

    Initial Commit

---
+++ MindteckTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindteckTask
+//
+//  Created by Anil Pahadiya on 03/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
